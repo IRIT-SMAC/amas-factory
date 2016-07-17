8d58908dd9757514b882c3ad7d64dce687297d19
Marjorie Salvetat <marjorie.salvetat@gmail.com>
2016-07-17 20:54:58 +0200

    Deletion of a useless serialuid

--- src/main/java/fr/irit/smac/amasfactory/agent/impl/Knowledge.java
+++ src/main/java/fr/irit/smac/amasfactory/agent/impl/Knowledge.java
@@ -25,5 +25,4 @@ import fr.irit.smac.amasfactory.agent.IKnowledge;
 
 public abstract class Knowledge implements IKnowledge{
 
-    private static final long serialVersionUID = -3116516422248728100L;
 }
